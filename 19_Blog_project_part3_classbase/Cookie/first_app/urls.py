476fde5c66b72dbea7655830e6d51e431e69d25c
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-14 22:51:37 +0600

    19 - project cookie :- introduction to cookie in django

--- 19_Blog_project_part3_classbase/Cookie/first_app/urls.py
+++ 19_Blog_project_part3_classbase/Cookie/first_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('get/',views.get_cookie, name='get_cookie'),
+    path('delete/',views.delete_cookies, name='delete_cookies'),
 ]
--- 19_Blog_project_part3_classbase/Cookie/first_app/views.py
+++ 19_Blog_project_part3_classbase/Cookie/first_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from datetime import datetime, timedelta
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    response = render(request, 'home.html')
+    # response.set_cookie('name', 'Rahim', max_age=10)
+    # 7 days ar jonne cookie save hoye thakbe.
+    response.set_cookie('name', 'Rahim', expires=datetime.utcnow()+timedelta(days=7))
+    return response
+
+def get_cookie(request):
+    name = request.COOKIES.get('name')
+    return render(request, 'get_cookie.html', {'name': name})
+
+
+def delete_cookies(request):
+    response = render(request, 'delete.html')
+    response.delete_cookie('name')
+    return response
