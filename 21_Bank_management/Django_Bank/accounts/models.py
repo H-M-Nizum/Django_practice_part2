c2856d36cdfe59f24a9e776920082d59039ca9e3
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-23 23:03:33 +0600

    21- Bank project :- 21-1 Creating Registration Models

--- 21_Bank_management/Django_Bank/accounts/models.py
+++ 21_Bank_management/Django_Bank/accounts/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+# choices ar data list ar modde list akare othoba tuppal ar modde tuppol akare rakha jay.
+ACCOUNT_TYPE = (
+    # ('frontend', 'backend'),
+    ('Savings', 'Savings'),
+    ('Current', 'Current'),
+)
+GENDER_TYPE = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
+
+
+class UserBankAccountModel(models.Model):
+    # user built in model, user ar modde ache username, firstname, lastname, email, password etc.
+    #  user ar sathe amader moel ar onetoone relation built korchi . 
+    user = models.OneToOneField(User, related_name='account', on_delete=models.CASCADE)
+    
+    account_type = models.CharField(max_lenght=10, choices=ACCOUNT_TYPE)
+    
+    # Every user have a unique account number.
+    account_no = models.IntegerField(unique=True)
+    
+    birth_day  =models.DateField(null=True, blank=True)
+    gender = models.CharField(max_lenght=10, choices=GENDER_TYPE)
+    
+    initial_deposite_date = models.DateField(auto_now_add=True)
+    
+    # akjon user 12 digit porjonto and dosomik ar por 2 gor nite parbe.
+    balance = models.DecimalField(default=0, max_length=12, decimal_places=2)
+    
+    
+class UserAddressModel(models.Model):
+    user = models.OneToOneField(User, related_name='address', on_delete=models.CASCADE)
+    
+    street_address = models.CharField(max_length = 100)
+    city = models.CharField(max_length = 100)
+    postal_code = models.IntegerField()
+    country = models.CharField(max_length = 100)
+    
+    
+
+
