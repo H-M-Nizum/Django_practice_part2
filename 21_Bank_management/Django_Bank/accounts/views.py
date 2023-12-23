11505d494aff07dacce1fa0d353f54b3a110dd0b
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-24 00:16:00 +0600

    21 - Bank project :- Creating our Views 21-4

--- 21_Bank_management/Django_Bank/accounts/views.py
+++ 21_Bank_management/Django_Bank/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .forms import UserRegistrationForm
+from django.contrib.auth import login
 
 # Create your views here.
+
+class UserRegistrationViews(FormView):
+    template_name = ''
+    form_class = UserRegistrationForm
+    success_url = ''
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(user)
+        return super().form_valid(form) # form_valid functions call hobe jodi sob thik thake
+    
