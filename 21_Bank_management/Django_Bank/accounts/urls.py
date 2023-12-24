ffc0eaf0e20b4a38dfacb7de11d03eac6165ab04
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-24 23:13:12 +0600

    21 - Bank project :- Handling profile page 21-11

--- 21_Bank_management/Django_Bank/accounts/urls.py
+++ 21_Bank_management/Django_Bank/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
    path('register/',  views.UserRegistrationViews.as_view(), name='register'),
    path('login/',  views.Userloginviews.as_view(), name='login'),
    path('logout/',  views.userlogoutview.as_view(), name='logout'),
+   path('profile/',  views.UserBankAccountUpdateView.as_view(), name='profile'),
 ]
