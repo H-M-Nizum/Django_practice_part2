391f228173a4c6dfec2df655207e263a5f41e790
H-M-Nizum <hmnizum1714032@gmail.com>
2023-12-24 22:38:16 +0600

    21-Bank project :- Login and Logout 21-10

--- 21_Bank_management/Django_Bank/accounts/views.py
+++ 21_Bank_management/Django_Bank/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import UserRegistrationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView, LogoutView
+
 # Create your views here.
 
 class UserRegistrationViews(FormView):
@@ -15,3 +18,16 @@ class UserRegistrationViews(FormView):
         login(self.request, user)
         return super().form_valid(form) # form_valid functions call hobe jodi sob thik thake
     
+    
+    
+class Userloginviews(LoginView):
+    template_name = 'user_login.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('home')
+    
+class userlogoutview(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy('home')
